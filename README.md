a153dd92e1f230f7531b35c7e48f1358d6536abb
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2023-05-02 13:17:54 +0700

    📝 docs: added readme file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dotfiles
+
+Personal dotfiles managed by [chezmoi](https://chezmoi.io).
+
+## Install
+
+This script will install Homebrew and chezmoi.
+After installation `chezmoi` will be initialized.
+
+```shell
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/yusoofsh/dotfiles/HEAD/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+echo ""
+echo "🤚  This script will setup .dotfiles for you."
+read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n'
+
+# Install Homebrew
+command -v brew >/dev/null 2>&1 ||
+	(echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+
+# Install chezmoi
+command -v chezmoi >/dev/null 2>&1 ||
+	(echo '👊  Installing chezmoi' && brew install chezmoi)
+
+# Initialize dotfiles
+
+(echo '🚀  Initializing dotfiles' && chezmoi init --apply --verbose yusoofsh)
+
+echo ""
+echo "✅  Done."
