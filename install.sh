0dbdfbac327e60fb5d6f13987e89a1ff7d9557c3
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2023-05-07 07:07:44 +0700

    Update home/.gitconfig Update install.sh Change attributes of home/before-install-packages.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,18 @@ echo "🤚  This script will setup .dotfiles for you."
 read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
 
 # Install Homebrew
+echo ""
 command -v brew >/dev/null 2>&1 ||
-	(echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+	(echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/opt/homebrew/bin/brew shellenv)")
 
 # Install chezmoi
+echo ""
 command -v chezmoi >/dev/null 2>&1 ||
 	(echo '👊  Installing chezmoi' && brew install chezmoi)
 
 # Initialize dotfiles
-(echo '🚀  Initializing dotfiles' && chezmoi init yusoofsh --apply --verbose --source "/Users/yusoofsh/Documents/Division/Open/Code/yusoofsh/dotfiles")
+echo ""
+(echo '🚀  Initializing dotfiles' && chezmoi init yusoofsh --apply --verbose)
 
 echo ""
 echo "✅  Done."
