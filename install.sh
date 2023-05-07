bf880afea4864b706585e3e5b683243617c01dc4
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2023-05-07 09:58:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,22 +3,30 @@
 set -eufo pipefail
 
 echo ""
-echo "🤚  This script will setup .dotfiles for you."
-read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
+echo "🤚 This script will setup .dotfiles for you."
+read -n 1 -r -s -p $'\n    Press any key to continue or Ctrl+C to abort...\n
+'
 
-# Install Homebrew
-echo ""
-command -v brew >/dev/null 2>&1 ||
-	(echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/opt/homebrew/bin/brew shellenv)")
+# Check if Homebrew is installed, if not install it
+if ! command -v brew &> /dev/null
+then
+    echo ""
+    echo "🍺 Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
-# Install chezmoi
-echo ""
-command -v chezmoi >/dev/null 2>&1 ||
-	(echo '👊  Installing chezmoi' && brew install chezmoi)
+# Check if chezmoi is installed, if not install it
+if ! command -v chezmoi &> /dev/null
+then
+    echo ""
+    echo "👊 Installing chezmoi"
+    brew install chezmoi
+fi
 
 # Initialize dotfiles
 echo ""
-(echo '🚀  Initializing dotfiles' && chezmoi init --apply --verbose yusoofsh)
+echo "🚀 Initializing dotfiles"
+chezmoi init --apply --verbose yusoofsh
 
 echo ""
-echo "✅  Done."
+echo "✅ Done."
