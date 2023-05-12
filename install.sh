46e071a91816794411281131cc559cfc33e2cbf6
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2023-05-12 21:48:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,24 +10,19 @@ read -n 1 -r -s -p $'\n    Press any key to continue or Ctrl+C to abort...\n'
 if ! command -v brew &>/dev/null; then
     echo ""
     echo "🍺 Installing Homebrew and it's dependencies"
-
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
     if [[ $(uname -s) == "Darwin" ]]; then
         eval "$(/opt/homebrew/bin/brew shellenv)"
     elif [[ $(uname -s) == "Linux" ]]; then
         eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
         if [[ $(lsb_release -si) == "Ubuntu" ]]; then
             echo ""
             echo "📦 Installing packages for Ubuntu"
-
             sudo apt-get update
             sudo apt-get install -y build-essential procps curl file git
         elif [[ $(lsb_release -si) == "Fedora" ]]; then
             echo ""
             echo "📦 Installing packages for Fedora"
-
             sudo dnf groupinstall -y 'Development Tools'
             sudo dnf install -y procps-ng curl file git
         else
