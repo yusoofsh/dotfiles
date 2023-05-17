9596fb3ece7701bec354415a82b85d44fe0cda5a
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2023-05-17 20:42:04 +0700

    Update home/.script/up.sh

--- home/dot_script/executable_up.sh
+++ home/dot_script/executable_up.sh
@@ -21,7 +21,7 @@ chezmoi update
 
 # Send a notification when updates are done
 if [ $? -eq 0 ]; then
-    terminal-notifier -title "Updates" -message "All updates completed."
+    terminal-notifier -title "`up.sh` Result" -message "All updates completed."
 else
-    terminal-notifier -title "Updates" -message "Updates failed. Check logs."
+    terminal-notifier -title "`up.sh` Result" -message "Updates failed. Check logs."
 fi
