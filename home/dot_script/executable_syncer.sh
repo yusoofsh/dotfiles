6a85f0c0bca4094563c64cf215f44cd6b5cbc02a
Yusoof Moh <18055365+yusoofsh@users.noreply.github.com>
2023-12-17 04:10:52 +0700

    Update home/.config/sheldon/plugins.toml Update home/.gitconfig Update home/.script/syncer.sh Update home/Library/Application Support/Code/User/settings.json Update home/Library/Preferences/com.googlecode.iterm2.plist Update home/Library/Preferences/com.knollsoft.Rectangle.plist Update home/Library/Preferences/com.lwouis.alt-tab-macos.plist Update home/Library/Preferences/com.raycast.macos.plist Add home/.bash_profile Add home/.bashrc Update home/.fly/state.yml Update home/.zprofile Update home/.zshrc Remove home/.bash_profile Remove home/.bashrc

--- home/dot_script/executable_syncer.sh
+++ home/dot_script/executable_syncer.sh
@@ -1,33 +1,16 @@
-#!/opt/homebrew/bin/zsh
+#!/bin/bash
 
-# Update macOS applications
-echo "Updating macOS applications..."
+echo "Updating App Store applications..."
 mas upgrade
 
-# Update Homebrew package list
-echo "Updating Homebrew package list..."
-brew update
+echo "Upgrading Homebrew packages and update the .Brewfile..."
+brew update && brew upgrade && brew cleanup && brew bundle dump --global --describe --force
 
-# Upgrade all installed Homebrew packages
-echo "Upgrading installed Homebrew packages..."
-brew upgrade
-
-# Remove outdated or unused Homebrew dependencies
-echo "Cleaning up Homebrew..."
-brew cleanup
-
-# Update Sheldon's locked packages
-echo "Updating Sheldon and modules..."
+echo "Updating Sheldon plugins..."
 sheldon lock --update
 
-# Dump the current state of installed Homebrew packages with descriptions (global scope and forced refresh)
-echo "Dumping installed Homebrew packages..."
-brew bundle dump --global --describe --force
+echo "Updating TLDR caches..."
+tldr --update
 
-# Re-add any deleted or modified dotfiles from Chezmoi
 echo "Re-adding Chezmoi dotfiles..."
-chezmoi re-add
-
-# Update Chezmoi and apply any changes to dotfiles
-echo "Updating Chezmoi dotfiles..."
-chezmoi update
+chezmoi re-add;
