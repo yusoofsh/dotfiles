abe7270e712125e4bda0c80a57f0649fe5901722
Yusoof Moh <18055365+yusoofsh@users.noreply.github.com>
2023-12-18 20:53:02 +0700

    Update home/.script/syncer.sh Update home/.Brewfile

--- home/dot_script/executable_syncer.sh
+++ home/dot_script/executable_syncer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Updating App Store applications..."
-mas upgrade
-
 echo "Upgrading Homebrew packages and update the .Brewfile..."
 brew update && brew upgrade && brew cleanup && brew bundle dump --global --describe --force
+
+echo "Updating App Store applications..."
+mas upgrade
 
 echo "Updating Sheldon plugins..."
 sheldon lock --update
@@ -13,4 +13,4 @@ echo "Updating TLDR caches..."
 tldr --update
 
 echo "Re-adding Chezmoi dotfiles..."
-chezmoi re-add;
+chezmoi re-add
