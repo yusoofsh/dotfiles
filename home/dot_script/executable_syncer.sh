d2cf2d833259a0be43b83e7778ac82ac6fd55754
Yusoof Moh <me@yusoofsh.id>
2024-10-16 16:03:41 +0700

    Update home/.dart-tool/dart-flutter-telemetry-session.json Update home/.script/syncer.sh Update home/Library/Developer/Xcode/UserData/IDEEditorInteractivityHistory Update home/Library/Application Support/coconutBattery/Mac.ccbarchive Update home/Library/Application Support/zoxide/db.zo Update home/.vnc/identities Update home/.vnc/ViewerStore/91517d58-0045-425b-b6e7-dc401902bfb5.jrle Update home/.vnc/ViewerStore/91517d58-0045-425b-b6e7-dc401902bfb5.vnc Update home/.vnc/config.d/vncviewer.d/CloudCredentials.bed Update home/.vnc/config.d/vncviewer.d/CloudSyncCache.bed Update home/.vnc/config.d/vncviewer.d/passwords.json

--- home/dot_script/executable_syncer.sh
+++ home/dot_script/executable_syncer.sh
@@ -2,25 +2,9 @@
 
 echo "Upgrading Homebrew packages and update the .Brewfile..."
 brew update && brew upgrade && brew cleanup && brew bundle dump --global --describe --force
-brew upgrade bottom keka latest neovim sheldon starship timemachineeditor
-
-echo "Updating App Store applications..."
-mas upgrade
-
-echo "Updating Volta tools..."
-volta install node@latest pnpm@latest yarn@latest
-
-echo "Updating Visual Studio Code extensions..."
-code --update-extensions
 
 echo "Updating Sheldon plugins..."
 sheldon lock --update
-
-echo "Updating TLDR caches..."
-tldr --update
-
-echo "Upgrading Flutter..."
-flutter upgrade
 
 echo "Re-adding Chezmoi dotfiles..."
 chezmoi init && chezmoi re-add --force
